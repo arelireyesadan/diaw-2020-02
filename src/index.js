272969c78656579e7bd214b09c4c1a24005410c4
arelireyesadan <areyesadan1812@gmail.com>
2021-01-19 07:48:45 -0600

    Materialize Working Test with Colors

--- src/index.js
+++ src/index.js
@@ -6,13 +6,3 @@
 // Importamos materialize
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize';
-
-console.log('Modern JS Working!! Thanks Babel ...');
-// let es solo una variable en el entorno en el que fue declarada
-// sino le mandan message, entonces mandará el saludo por defecto
-const show = (message = 'Saludo por defecto') => {
-  alert(message);
-};
-// Ejemplo
-show();
-show('Mi saludo');
